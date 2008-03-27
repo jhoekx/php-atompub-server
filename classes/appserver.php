7b7de8bdcd86147ddf93fdfc58aec7bea643e810
Jeroen Hoekx <jeroen@hoekx.be>
2008-03-27 17:08:00 +0000

    Added basic categories support possibilities.

--- classes/appserver.php
+++ classes/appserver.php
@@ -5,6 +5,7 @@ require_once("appuritemplate.php");
 
 require_once("appservicedoc.php");
 require_once("appcollection.php");
+require_once("appcategory.php");
 
 require_once("httpexception.php");
 
@@ -23,6 +24,8 @@ class App_Server {
 		$templates = array(
 			array("App_Collection", "{colname}/"),
 			array("App_Collection", "{colname}/pages/{nr}"),
+			array("App_Category", "-/{category}/"),
+			array("App_Category", "-/{category}/pages/{nr}"),
 			array("App_Entry", "{colname}/{entryname}"),
 			array("App_Entry", "{colname}/{year}/{month}/{day}/{entryname}"),
 			array("App_Servicedoc", "service"),
@@ -50,8 +53,7 @@ class App_Server {
 		switch ($obj) {
 			case "App_Collection":
 				$service = new App_Servicedoc("service.xml", $this->base_uri);
-				$collection = $this->create_collection($vars["colname"], $service);
-				$collection->uri = $uri;
+				$collection = $this->create_collection($vars, $service);
 				return $collection;
 				break;
 			case "App_Servicedoc":
@@ -60,23 +62,36 @@ class App_Server {
 				break;
 			case "App_Entry":
 				$service = new App_Servicedoc("service.xml", $this->base_uri);
-				$collection = $this->create_collection($vars["colname"], $service);
+				$collection = $this->create_collection($vars, $service);
 				
 				$entry = $collection->get_entry($uri);
 				return $entry;
+				break;
+			case "App_Category":
+				$service = new App_Servicedoc("service.xml", $this->base_uri);
+				$category = new App_Category($uri, $this->store, $service);
+				return $category;
 				break;
 			default:
 				throw new Exception("No resource!");
 		}
 	}
 	
-	public function create_collection($name, $service) {
+	public function create_collection($vars, $service) {
+		$name = $vars["colname"];
+		
+		if ( !array_key_exists("nr",$vars) ) {
+			$uri = new URI($this->base_uri.$name."/");
+		} else {
+			$uri = new URI($this->base_uri.$name."/pages/".$vars["nr"]);
+		}
+	
 		if ( file_exists("templates/collection_".$name.".php") ) {
 			include_once("templates/collection_".$name.".php");
 			$class = "App_Collection_".$name;
-			$collection = new $class($name, $this->store, $service);
+			$collection = new $class($uri, $this->store, $service);
 		} else {
-			$collection = new App_Collection($name, $this->store, $service);
+			$collection = new App_Collection($uri, $this->store, $service);
 		}
 		
 		return $collection;
